852ef3fc6fc41945bc059659f7891dbf4e54c25b
Marcel Rico <personal.mr4@gmail.com>
2019-12-07 21:26:19 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -18,9 +18,8 @@ def get_user_input
   gets
 end
 
-def end_game
-  card_total = 
-  puts "Sorry, you hit #{}. Thanks for playing!"
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
